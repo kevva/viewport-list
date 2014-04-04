17736100f9ee084740db48c034f675365af052a5
Kevin Martensson <kevinmartensson@gmail.com>
2014-04-04 13:54:24 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var got = require('got');
+
+/**
+ * Return a list of devices and their viewports
+ *
+ * @param {String|Array} item
+ * @param {Function} cb
+ * @api public
+ */
+
+module.exports = function (item, cb) {
+    var ret = [];
+
+    got('http://viewportsizes.com/devices.json', function (err, res) {
+        if (err) {
+            return cb(err);
+        }
+
+        var sizes = JSON.parse(res);
+        var arr = sizes.filter(function (size) {
+            return size['Device Name'].split(' ').join('').toLowerCase().indexOf(item.toLowerCase()) !== -1;
+        });
+
+        if (arr.length === 0) {
+            return cb('Couldn\'t get any items');
+        }
+
+        arr.forEach(function (item) {
+            ret.push({
+                name: item['Device Name'].split(' ').join('').toLowerCase(),
+                platform: item.Platform,
+                os: item['OS Version'],
+                size: item['Portrait Width'] + 'x' + item['Landscape Width'],
+                release: item['Release Date']
+            });
+        });
+
+        cb(null, ret);
+    });
+};
